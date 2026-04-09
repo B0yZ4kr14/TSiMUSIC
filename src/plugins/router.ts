music-assistant/frontend

Remove unnecessary localStorage fallback for startup_view and add migration TODOs
startup_view was never released to main, so no users have it in localStorage —
remove the fallback in router.ts and FrontendConfig.vue as flagged in PR review.

Add TODO comments to the remaining localStorage fallbacks (theme, language,
menu_items) that do need migration, so they can be cleaned up later.

https://claude.ai/code/session_012JvFPvHDP26yi54DjhBiQs

src/plugins/router.ts
@@ -11,11 +11,8 @@ const routes = [
       {
         path: "",
         redirect: () => {
-          // Read from user preferences first, fallback to localStorage for migration
           const startupView =
-            store.currentUser?.preferences?.startup_view ||
-            localStorage.getItem("frontend.settings.startup_view") ||
-            "home";
+            store.currentUser?.preferences?.startup_view || "home";
           return `/${startupView}`;
         },
       },

B0yZ4kr14/TSiMUSIC

src/plugins/router.ts
@@ -79,11 +79,8 @@ const routes: RouteRecordRaw[] = [
       {
         path: "",
         redirect: () => {
-          // Read from user preferences first, fallback to localStorage for migration
           const startupView =
-            store.currentUser?.preferences?.startup_view ||
-            localStorage.getItem("frontend.settings.startup_view") ||
-            "discover";
+            store.currentUser?.preferences?.startup_view || "discover";
           return `/${startupView}`;
         },
       },